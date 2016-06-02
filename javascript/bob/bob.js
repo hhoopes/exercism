860415fbb8ceeb72089c573b81671559563d758b
hhoopes <heidih@gmail.com>
2016-06-01 21:32:08 -0600

    Refactor Bob with simpler method for checking for numbers

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,20 +1,15 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  var inputCaseBool = isUpperCase(input)
-  if (input.length === 0 || !input.trim()) { return response[3] }
-  else if (input.slice(-1) === "?") {
-    return (inputCaseBool) ? response[0] : response [1]
+  if (!input.trim()) { return response[3] }
+  else if (input === input.toUpperCase() && input !== input.toLowerCase()) {
+    return response[0];
   }
-  else if (input.slice(-1) === "!" || inputCaseBool) {
-    return (/\d/.test(input) || inputCaseBool) ? response[0] : response[2]
+  else if (input.slice(-1) === "?") {
+    return response[1];
   }
   else { return response[2] }
 }
-
-var isUpperCase = function(string) {
-  return string.toUpperCase() === string && /[A-Za-z]/.test(string);
-};
 
 const response = {
   0: "Whoa, chill out!",
--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,11 +1,12 @@
-var Hamming = function() {
-};
+var Hamming = function() {};
 
 Hamming.prototype.compute = function(strand_1, strand_2) {
   var strand_length = strand_1.length;
   if (strand_length != strand_2.length){
     throw new Error("DNA strands must be of equal length.");
-  } else {
+  }
+
+  else {
     var difference = 0;
     for (var i = 0; i < strand_length; i++) {
       if (strand_1[i] != strand_2[i]){
