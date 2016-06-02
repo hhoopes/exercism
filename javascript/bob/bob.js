f952ea401a083bb1ab629c4b8401fcea0c1f7cfb
hhoopes <heidih@gmail.com>
2016-06-01 21:20:33 -0600

    Remove unneeded require

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,9 +1,3 @@
-var _ = require("lodash");
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
