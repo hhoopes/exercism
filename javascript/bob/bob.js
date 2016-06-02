78299a6d699cb78b389acda6cd34f866a7fdabd0
hhoopes <heidih@gmail.com>
2016-06-01 21:18:28 -0600

    Add Bob JS solution

---
+++ javascript/bob/bob.js
@@ -0,0 +1,32 @@
+var _ = require("lodash");
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+  var inputCaseBool = isUpperCase(input)
+  if (input.length === 0 || !input.trim()) { return response[3] }
+  else if (input.slice(-1) === "?") {
+    return (inputCaseBool) ? response[0] : response [1]
+  }
+  else if (input.slice(-1) === "!" || inputCaseBool) {
+    return (/\d/.test(input) || inputCaseBool) ? response[0] : response[2]
+  }
+  else { return response[2] }
+}
+
+var isUpperCase = function(string) {
+  return string.toUpperCase() === string && /[A-Za-z]/.test(string);
+};
+
+const response = {
+  0: "Whoa, chill out!",
+  1: "Sure.",
+  2: "Whatever.",
+  3: "Fine. Be that way!"
+}
+
+module.exports = Bob;
