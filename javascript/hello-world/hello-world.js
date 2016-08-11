38f1db503ed7fcc249458b813bb4d91387e55a68
hhoopes <heidih@gmail.com>
2016-08-11 11:34:46 -0600

    Add new exercisms puzzles for when offline

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -7,11 +7,7 @@
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(input) {
-  if (input == ""){
-    return "Hello, World!";
-  } else {
-    return "Hello, " + input + "!";
-  }
+  return input == ""? "Hello, World!" : "Hello, " + input + "!"
 };
 
 module.exports = HelloWorld;
--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,14 @@
 class Hamming
-  def self.compute(strand_one, strand_two)
-    raise ArgumentError unless strand_one.length == strand_two.length
-    strand_one.split("").zip(strand_two.split("")).reject do | pair |
-      pair.first == pair.last
-    end.length
-  end
-end                                                               
+  def calculate_difference(strand_1, strand_2)
+    strand_1.split("").zip(strand_2.split("")).reject do |pair|  # => [["A", "A"], ["C", "C"], ["G", "G"], ["T", "T"]], [["A", "A"], ["C", "C"], ["C", "G"], ["T", "T"]], [["A", "A"], ["C", "C"], ["C", "G"], ["A", "T"]], [["A", "A"], ["C", "C"], ["G", "G"], ["T", "T"], ["A", "C"], ["C", "G"], ["G", "T"], ["T", "A"], ["A", "C"], ["C", "T"], ["T", "A"], ["C", "C"], ["A", "G"]]
+      pair.first == pair.last                                    # => true, true, true, true, true, true, false, true, true, true, false, false, true, true, true, true, false, false, false, false, false, false, false, true, false
+    end.length                                                   # => 0, 1, 2, 8
+  end                                                            # => :calculate_difference
+
+end  # => :calculate_difference
+
+hamming = Hamming.new                                           # => #<Hamming:0x007fe62b971698>
+hamming.calculate_difference("ACGT", "ACGT")                    # => 0
+hamming.calculate_difference("ACCT", "ACGT")                    # => 1
+hamming.calculate_difference("ACCA", "ACGT")                    # => 2
+hamming.calculate_difference("ACGTACGTACTCA", "ACGTCGTACTACG")  # => 8
