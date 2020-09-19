4a22e45ce83a9955a1e78094612b0942c655180e
Heidi Hoopes <heidih@gmail.com>
2020-09-19 14:37:30 -0400

    Add allergies excercise

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -1,29 +1,26 @@
-//
-// This is only a SKELETON file for the 'Allergies' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Allergies {
-  constructor(allergenValue) {
-    this.allergenValue = allergenValue;
-    this.allergenRepresentation = binaryRepresentation(this.allergenValue);
-    this.allergenMap = {
-      "eggs": 0,
+  constructor(userScore) {
+    this.userScore = userScore;
+    this.allergenList = {
+      eggs          : 1,
+      peanuts       : 2,
+      shellfish     : 4,
+      strawberries  : 8,
+      tomatoes      : 16,
+      chocolate     : 32,
+      pollen        : 64,
+      cats          : 128,
     };
   }
 
   list() {
-    throw new Error("Remove this statement and implement this function");
+   return Object.keys(this.allergenList).filter(allergen => {
+     return this.allergicTo(allergen)
+   });
   }
 
-  allergicTo(foodAllergen) {
-    if(this.allergenValue == 0) {
-      return false
-    };
-    return this.allergenRepresentation & binaryRepresentation(this.allergenMap[foodAllergen])
+  allergicTo(potentialAllergen) {
+    const allergenValue = this.allergenList[potentialAllergen];
+    return Boolean(allergenValue & this.userScore);
   }
-}
-
-function binaryRepresentation(int) {
-  return int.toString(2);
 }
