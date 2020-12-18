a214cb2fd0d83b95e9a0b6deb2109a6d83f42d72
Heidi Hoopes <heidih@gmail.com>
2020-12-17 20:37:19 -0700

    Add solution for scrabble and new solution for matrix

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,23 +1,19 @@
 export class Matrix {
   constructor(matrixDescriptor) {
-    this.numericRows = this.getNumericRows(matrixDescriptor)
-  }    
-
-  getNumericRows(matrixDescriptor) {
-    return matrixDescriptor.split('\n').map(row => row.split(' ').map(Number)) 
-  }
+    this.numericRows = matrixDescriptor.split('\n')
+      .map(row => row.split(' ').map(Number)); 
+  };
 
   get rows() {
     return this.numericRows;
   }
 
   get columns() {
-    const numOfCols = this.numericRows[0].length;
+    const numCols = this.numericRows[0].length;
     let cols = [];
-    [...Array(numOfCols)].map((_, i) => { 
-      cols[i] = this.numericRows.map(row => row[i]) 
-    })
-
-    return cols
+    for (var i = 0; i < numCols; i++) {
+      cols[i] = this.numericRows.map(row => row[i]);
+    }
+    return cols;
   }
 }
